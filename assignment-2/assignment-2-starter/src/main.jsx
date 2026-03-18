ad6a39d99c8cc649ceb66d1ba46e8256f4da4a4d
user <user@users-MacBook-Air.local>
2026-03-18 16:42:55 -0400

    organize assignment 3 into folder and remove node_modules

--- assignment-2/assignment-2-starter/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import App from './App.jsx'
-
-const root = document.getElementById('root')
-
-createRoot(root).render(<App />)
